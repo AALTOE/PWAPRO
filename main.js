5d3c6a49f8231235c0808cd32f075726df6fcb62
Alex Alto Espiridion <48866876+OWLSALEX@users.noreply.github.com>
2020-06-08 20:01:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,41 +2,46 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
       navigator.serviceWorker.register('sw.js').then(function(registration) {
         // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log('Registro de ServiceWorker exitoso con alcance: ', registration.scope);
       }, function(err) {
         // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
+        console.log('El registro de ServiceWorker falló: ', err);
       });
     });
   }
 
-  let deferredPrompt;
-  var div = document.getElementById("add-to");
-  div.style.display = 'none';
+  const divInstall = document.getElementById('installContainer');
+  const butInstall = document.getElementById('butInstall');
 
-  window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-
-    deferredPrompt = e;
-
-    div.style.display = 'block';
+  window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('👍', 'beforeinstallprompt', event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container
+    divInstall.classList.toggle('hidden', false);
   });
 
-  div.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    div.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
+  butInstall.addEventListener('click', () => {
+    console.log('👍', 'Botón de instalacion precionado');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    promptEvent.userChoice.then((result) => {
+      console.log('👍', 'Elección del usuario', result);
+      // Reset the deferred prompt variable, since
+      // prompt() can only be called once.
+      window.deferredPrompt = null;
+      // Hide the install button.
+      divInstall.classList.toggle('hidden', true);
+    });
   });
 
+  window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'App instalada', event);
+  });
   
